e38576b003dedf4c8b132b9955387f740bd8ccb0
Silence-GitHub <lukaibolkb@gmail.com>
2016-05-04 23:58:13 +0800

    Initial Commit

---
+++ SWSegmentedController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWSegmentedController
+//
+//  Created by 陆凯波 on 16/5/4.
+//  Copyright © 2016年 Kaibo Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
